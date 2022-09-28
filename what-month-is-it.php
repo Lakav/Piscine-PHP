6e8bffd00283bebeb3f7077745f9982e01cb25e9
julescavanier <jules.cavanier@ynov.com>
2022-09-28 11:24:52 +0200

    q2

---
+++ what-month-is-it.php
@@ -0,0 +1,53 @@
+<?php
+$month = 9;
+switch ($month){
+    case 1 : if ($month = 1){
+        echo "We are in the month of January";
+        break;
+    }
+    case 2 : if ($month = 2) {
+        echo "We are in the month of February";
+        break;
+    }
+    case 3 : if ($month = 3) {
+        echo "We are in the month of March";
+        break;
+    }
+    case 4 : if ($month = 4) {
+        echo "We are in the month of April";
+        break;
+    }
+    case 5 : if ($month = 5) {
+        echo "We are in the month of May";
+        break;
+    }
+    case 6 : if ($month = 6) {
+        echo "We are in the month of June";
+        break;
+    }
+    case 7 : if ($month = 7) {
+        echo "We are in the month of July";
+        break;
+    }
+    case 8 : if ($month = 8) {
+        echo "We are in the month of August";
+        break;
+    }
+    case 9 : if ($month = 9) {
+        echo "We are in the month of September";
+        break;
+    }
+    case 10 : if ($month = 10) {
+        echo "We are in the month of October";
+        break;
+    }
+    case 11 : if ($month = 11){
+        echo "We are in the month of November";
+        break;
+    }
+    case 12 : if ($month = 12){
+        echo "We are in the month of December";
+        break;
+    }
+}
+?>
