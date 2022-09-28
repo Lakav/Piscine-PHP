e59b73cbaab3387e10c2b262b2a1e9739908422a
julescavanier <jules.cavanier@ynov.com>
2022-09-28 09:25:29 +0200

    Q1

---
+++ primitive-declarations.php
@@ -0,0 +1,11 @@
+<?php
+
+$firstStr = ":)";
+$firstEscapeStr =":(\n";
+$firstBool = true;
+$firstInteger = 1;
+$firstFloat = 1.1;
+$firstNull = Null;
+$firstArray = array(2, null, 'Hi', 5.5, false);
+
+?>
