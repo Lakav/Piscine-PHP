a1a6bc351583d0e041a181fba128b5e880667232
julescavanier <jules.cavanier@ynov.com>
2022-09-30 09:22:12 +0200

    ok

---
+++ pangram.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    function isPangram($param) {
+
+        $sentences = strtolower(trim($param));
+        $letters = str_split("thequickbrownfoxjumpsoverthelazydog");
+
+        foreach ($letters as $letter) {
+            if (!strstr($sentences, $letter))
+                return false;
+        }
+
+        return true;
+    }
+
+
+?>
