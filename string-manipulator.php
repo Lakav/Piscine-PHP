79c68425010919cf66fab936d1d0b45f46bae1e4
julescavanier <jules.cavanier@ynov.com>
2022-09-30 08:59:28 +0200

    ok

---
+++ string-manipulator.php
@@ -0,0 +1,28 @@
+<?php
+
+function capsMe(string $x) :string
+{
+return strtoupper($x);
+}
+
+function lowerMe(string $x) : string
+{
+return strtolower($x);
+}
+
+function upperCaseFirst(string $x) : string
+{
+return ucwords($x);
+}
+
+function lowerCaseFirst(string $x) : string
+{
+return lcfirst($x);
+}
+
+function removeBlankSpace(string)
+{
+
+}
+
+?>
