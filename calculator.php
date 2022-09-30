ea861ecb3ef31e4c8149c26addd6b4adbbf56e71
julescavanier <jules.cavanier@ynov.com>
2022-09-30 10:25:37 +0200

    ok

--- calculator.php
+++ calculator.php
@@ -1,8 +1,18 @@
 <?php
 
-function calc(string $str) : int
+function calc(string $equation) : int
 {
-    return eval($str);
+    $equation = preg_replace("/[^0-9+\-.*\/()%]/","",$equation);
+    $equation = preg_replace("/([+-])([0-9]+)(%)/","*(1\$1.\$2)",$equation);
+    // you could use str_replace on this next line
+    // if you really, really want to fine-tune this equation
+    $equation = preg_replace("/([0-9]+)(%)/",".\$1",$equation);
+    if ( $equation == "" ) {
+        $return = 0;
+    } else {
+        eval("\$return=" . $equation . ";" );
+    }
+    return $return;
 }
 
 ?>
